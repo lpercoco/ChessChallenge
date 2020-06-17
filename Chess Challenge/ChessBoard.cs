0829d180f9f166e30796aa5ed262a1404f42fcbb
Lisandro Martin Percoco <lisandro.percoco@hotmail.com>
2020-06-17 14:20:57 -0700

    desarrollo incial del tablero

---
+++ Chess Challenge/ChessBoard.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess_Challenge
+{
+    public class ChessBoard
+    {
+        //improve
+        private List<string> columnsReference = new List<string>(new string[] { "A", "B", "C", "D", "E", "F", "G", "H" });
+        const int BOXSIZE = 8;
+
+        public void printChessBoard()
+        {
+            printLettersReference(2, 1);
+            printNumbersReference(1, 0);
+            printRows(3, 2);
+            printColumns(2, 2);
+        }
+
+        //printLettersReference(2,1);
+        public void printLettersReference(int x, int y)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Console.SetCursorPosition(x + (BOXSIZE * i) + (BOXSIZE / 2), y);
+                Console.Write(columnsReference[i]);
+            }
+        }
+
+        //printNumbersReference(1,1);
+        public void printNumbersReference(int x, int y)
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                Console.SetCursorPosition(x, y + ((BOXSIZE / 2) * i));
+                Console.Write(i);
+            }
+        }
+
+        //printColumns(2, 2);
+        public void printColumns(int x, int y)
+        {
+            int lineHeight = BOXSIZE * 4;
+            for (int lineNumber = 0; lineNumber < 9; lineNumber++)
+            {
+                for (int l = 1; l < (lineHeight); l++)
+                {
+                    Console.SetCursorPosition(x + (lineNumber * BOXSIZE), y + l);
+                    Console.Write("|");
+                }
+            }
+        }
+
+        //printRows(3, 2);
+        public void printRows(int x, int y)
+        {
+            int hyphensCount = 8 * BOXSIZE;
+            string hyphens = new string('-', hyphensCount);
+
+            for (int lineNumber = 0; lineNumber < 9; lineNumber++)
+            {
+                Console.SetCursorPosition(x, y + ((BOXSIZE / 2) * lineNumber));
+                Console.Write(hyphens.ToString());
+            }
+        }
+    }
+}
---
+++ Chess Challenge/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chess_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChessBoard myChessBoard = new ChessBoard();
+            myChessBoard.printChessBoard();
+            Console.Read();
+        }
+        
+
+    }
+}
